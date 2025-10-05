41fcadba83c98459059d4164e88e532693441dcb
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-10-05 14:04:54 -0400

    Build

--- app/config.ts
+++ app/config.ts
@@ -4,7 +4,7 @@ import { getGlobalConfig } from '@embroider/macros/src/addon/runtime';
 
 const ENV = {
   environment: import.meta.env.DEV ? 'development' : 'production',
-  rootURL: '/',
+  rootURL: './',
   locationType: 'history',
   EmberENV: {},
   APP: {},
