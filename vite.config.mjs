f1da35aca1b9e04507fc5e6a7504bd305ec1e168
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-10-05 14:07:35 -0400

    Build

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,9 @@ import { extensions, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
+  // To match:
+  // https://nullvoxpopuli.github.io/ember-js-typetest/
+  base: '/ember-js-typetest/',
   plugins: [
     ember(),
     // extra plugins here
